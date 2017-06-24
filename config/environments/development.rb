bf7db1721103525c282f84673715f13d78e13284
thaivankhoa <thaivankhoa2802@gmail.com>
2017-06-24 18:11:08 +0700

    action cable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  #agile page 165
+  config.action_cable.disable_request_forgery_protection = true
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
